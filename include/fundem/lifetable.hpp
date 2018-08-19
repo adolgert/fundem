fe3a4754e9450b58e6f7548dc845f0cbac03f348
Andrew Dolgert <adolgert@uw.edu>
2018-08-19 16:40:00 -0700

    working C unit test for survival function

--- include/fundem/lifetable.hpp
+++ include/fundem/lifetable.hpp
@@ -1,22 +1,24 @@
 #ifndef FUNDEM_LIFETABLE_HPP
 #define FUNDEM_LIFETABLE_HPP
 
+#include <vector>
+
+
 namespace fundem {
 
-template<REAL> std::vector<REAL> FirstMomentSurvival(
+template<typename REAL>
+std::vector<REAL> FirstMomentSurvival(
         const std::vector<REAL>& mx, const std::vector<REAL>& ax, const std::vector<REAL>& nx)
 {
     std::vector<REAL> survival(mx.size());
+    auto survival_iter = survival.begin();
 
-    for (
-            const auto mxi = mx.begin(),
-            const auto axi = ax.begin(),
-            const auto nxi = nx.begin(),
-            auto survival_iter = survival.begin();
+    for (auto mxi = mx.begin(), axi = ax.begin(), nxi = nx.begin();
             mxi != mx.end();
             mxi++, axi++, nxi++, survival_iter++
-            ) {
-        *survival_iter = (1.0 - *mxi * *axi) / (1.0 + *mxi * (*nxi - *axi))
+            )
+    {
+        *survival_iter = (1.0 - *mxi * *axi) / (1.0 + *mxi * (*nxi - *axi));
     }
     return survival;
 }
--- tests/test_lifetable.cpp
+++ tests/test_lifetable.cpp
@@ -1,9 +1,44 @@
+#include <cmath>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "fundem/lifetable.hpp"
 
+
+using namespace fundem;
+
+double epsilon = 1e-9;
 
 // Simple test, does not use gmock
 TEST(Dummy, foobar)
 {
 EXPECT_EQ(1, 1);
+}
+
+
+/*! Tests survival calculation for constant mortality rate.
+ *
+ */
+TEST(FM_SURVIVAL_CONSTANT_MORTALITY, blah)
+{
+    std::vector<double> mx(23);
+    std::vector<double> ax(23);
+    std::vector<double> nx(23);
+    std::vector<double> expected(23);
+
+    double mortality_rate = 0.1;
+    double base_interval = 5;
+
+    for (auto i=0; i<23; i++) {
+        nx[i] = base_interval * (i+1)/23;
+        auto mean_age = (1 / mortality_rate) - nx[i] / (std::exp(mortality_rate * nx[i]) - 1);
+        mx[i] = mortality_rate;
+        ax[i] = mean_age;
+        expected[i] = std::exp(-nx[i] * mortality_rate);
+    }
+    auto answer = FirstMomentSurvival(mx, ax, nx);
+    EXPECT_EQ(answer.size(), mx.size());
+
+    for (auto check_idx=0; check_idx<23; check_idx++) {
+        EXPECT_LT(std::abs(answer[check_idx] - expected[check_idx]), epsilon);
+    }
 }
